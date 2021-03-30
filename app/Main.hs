9643f4750e9c857781fdf3450ebae2a35506622b
patrick brisbin <pbrisbin@gmail.com>
2021-03-30 14:02:26 -0400

    Disable main

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,12 @@ module Main
 
 import RIO
 
-import Lockit
-import Lockit.GitHub
-import Lockit.Time
+-- import Lockit
+-- import Lockit.GitHub
+-- import Lockit.Time
 
 main :: IO ()
-main = runSimpleApp $ do
-    cutoff <- liftIO $ subtractDays (Days 30) <$> getCurrentTime
-    run (mkOwnerName "pbrisbin") (mkRepoName "aurget") cutoff
+main = undefined
+-- main = runSimpleApp $ do
+--     cutoff <- liftIO $ subtractDays (Days 30) <$> getCurrentTime
+--     run (mkOwnerName "pbrisbin") (mkRepoName "aurget") cutoff
